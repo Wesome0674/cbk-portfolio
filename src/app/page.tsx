9ad9e3abb50905b3aa5f20b9ae7e533153aee940
Killian <killian.boularand@outlook.fr>
2024-12-17 08:00:50 +0100

    hehe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import AboutMe from "@/ui/components/AboutMe";
 import Header from "@/ui/components/Header";
 import NavBar from "@/ui/components/navigation/NavBar";
-import PassionSlider from "@/ui/components/PassionSlider";
+
 
 export default function Home() {
   return (
