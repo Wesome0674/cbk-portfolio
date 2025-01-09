aeb85f7ffadcee1fae6b5150d2666a8ddf26f2f6
Killian <killian.boularand@outlook.fr>
2025-01-09 21:09:18 +0100

    c bon ou pas

--- next.config.ts
+++ next.config.ts
@@ -9,13 +9,6 @@ const nextConfig: NextConfig = {
       },
     ],
   }, */
-  webpack(config) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      gsap: require.resolve('gsap'), // Assure-toi que Webpack résout gsap correctement
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
