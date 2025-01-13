4c0620f11eda385b7ddd514e22146f026a92288e
Killian <killian.boularand@outlook.fr>
2025-01-13 20:52:53 +0100

    traduction passion done

--- src/ui/components/PassionSliderClient.tsx
+++ src/ui/components/PassionSliderClient.tsx
@@ -2,18 +2,33 @@
 
 import Image from "next/image";
 import { Typographie } from "../design-system/Typographie";
-import PassionTitle from "./PassionTitle";
+import { useTranslations } from "next-intl";
+import { ImportantText } from "../design-system/ImportantText";
 
 type Passion = {
   id: number;
   name: string;
+  icon: string;
   translations: { translatedName: string }[];
 };
 
-export default function PassionSliderClient({ passions }: { passions: Passion[] }) {
+export default function PassionSliderClient({
+  passions,
+}: {
+  passions: Passion[];
+}) {
+  const t = useTranslations();
+  
   return (
     <div className="max-w-[408px] overflow-hidden border-t border-b border-primary gap-[13px] py-[20px] flex flex-col w-fit items-center">
-      <PassionTitle />
+      <ImportantText img="/img/svg/Marker.svg">
+        <Typographie
+          variant="h4"
+          className="font-calfine text-center uppercase"
+        >
+          {t("aboutMe.passions")}
+        </Typographie>
+      </ImportantText>
       <div className="overflow-hidden w-full">
         <div className="flex items-center gap-[14px] w-max animate-slider">
           {[...passions, ...passions].map((passion, index) => (
@@ -23,7 +38,7 @@ export default function PassionSliderClient({ passions }: { passions: Passion[] 
                   ? passion.translations[0].translatedName
                   : passion.name}
               </Typographie>
-              <Image alt="" src="/img/svg/travel.svg" width={24} height={24} />
+              <Image alt="" src={passion.icon} width={24} height={24} />
               <div className="w-[50px] h-[4px] bg-primary"></div>
             </div>
           ))}
