589ec90cfe7650f6da4647af3806c090046f2523
Killian <killian.boularand@outlook.fr>
2025-01-14 10:59:29 +0100

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,6 @@ export const config = {
     // - … if they start with `/api`, `/_next` or `/_vercel`
     // - … the ones containing a dot (e.g. `favicon.ico`)
     '/((?!api|_next|_vercel|.*\\..*).*)',
-    // However, match all pathnames within `/users`, optionally with a locale prefix
-    '/([\\w-]+)?/selectedWork/(.+)'
+   
   ]
 };
