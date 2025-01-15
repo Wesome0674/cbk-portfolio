be34a2927930cb45d8dded5f2aae089ce7fbcf8f
Killian <killian.boularand@outlook.fr>
2025-01-15 08:10:45 +0100

    Final

--- src/ui/components/Loader.tsx
+++ src/ui/components/Loader.tsx
@@ -7,7 +7,7 @@ const Loader = () => {
     <div className="w-screen h-screen fixed top-0 left-0 grid place-content-center z-[200] bg-primary">
       <div className="flex flex-col items-center gap-[70px]">
         <div className="relative loader aspect-video">
-          <Image src="/img/svg/logoLoader.svg" alt="" fill className="fixed" />
+          <Image src="/img/svg/LogoLoader.svg" alt="" fill className="fixed" />
         </div>
         <div
           className="animate-spin inline-block w-12 h-12 border-[3px] border-current border-t-transparent text-tercery rounded-full"
